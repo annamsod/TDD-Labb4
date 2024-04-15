a2a9e1f69dcf716d993947dadab700e479899e34
anna <anna.soderlund@ha.ax>
2024-04-15 11:37:28 +0300

    Added code and tests for add

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -10,9 +10,26 @@ public class StringCalculator {
         this.logger = logger;
     }
 
-    public int add(String input) {
-        // TODO use your previous implementation, and include the tests too!
-        // Also add logging necessary for Labb 4 exercise 1
-        return 0;
+    public int add(String input) throws IllegalArgumentException {
+        int sum=0;
+        if(!input.isEmpty()) {
+            String[] numbers;
+            if(input.startsWith("//")){
+                char divider = input.charAt(2);
+                String newString = input.substring(input.indexOf("\n")+1);
+                numbers = newString.split(String.valueOf(divider));
+            }
+            else {
+                numbers = input.split("[, \n]");
+            }
+            for (String number : numbers) {
+                int num = Integer.parseInt(number);
+                if(num<0){
+                    throw new IllegalArgumentException("Negatives not allowed: " + num);
+                }
+                sum += num;
+            }
+        }
+        return sum;
     }
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -15,4 +15,54 @@ public class StringCalculatorTest {
     public void testEmptyStringReturnsZero() {
         Assertions.assertEquals(0, calculator.add(""));
     }
+
+    @Test
+    public void testOneNumberReturnNumber(){
+        Assertions.assertEquals(5, calculator.add("5"));
+    }
+
+    @Test
+    public void testTwoNumbersReturnSum(){
+        Assertions.assertEquals(8,calculator.add("5,3"));
+    }
+
+    @Test
+    public void testWrongAnswer(){
+        Assertions.assertNotEquals(10,calculator.add("5,3"));
+    }
+
+    @Test
+    public void testThreeNumbers(){
+        Assertions.assertEquals(6,calculator.add("1,2,3"));
+    }
+
+    @Test
+    public void testTenNumbers(){
+        Assertions.assertEquals(10,calculator.add("1,1,1,1,1,1,1,1,1,1"));
+    }
+
+    @Test
+    public void testStringWithNewRow(){
+        Assertions.assertEquals(10,calculator.add("2\n5,3"));
+    }
+
+    @Test
+    public void testDifferentDivider(){
+        Assertions.assertEquals(3,calculator.add("//;\n1;2"));
+    }
+
+    @Test
+    public void testOnlyNegativeNumber(){
+        Exception exception = Assertions.assertThrows(IllegalArgumentException.class,
+                () -> calculator.add("-8"));
+        Assertions.assertEquals("Negatives not allowed: -8", exception.getMessage());
+    }
+
+    @Test
+    public void testNegativeNumber(){
+        Exception exception = Assertions.assertThrows(IllegalArgumentException.class,
+                () -> calculator.add("1,-2"));
+        Assertions.assertEquals("Negatives not allowed: -2", exception.getMessage());
+    }
+
 }
